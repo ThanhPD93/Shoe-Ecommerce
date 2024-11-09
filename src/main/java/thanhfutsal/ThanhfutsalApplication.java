1ace033379cd07e17b8d3bab0085ab0a920f94d8
Thanh Phạm <doanthanh1999vn@gmail.com>
2024-11-09 10:54:09 +0700

    Initial commit

---
+++ src/main/java/thanhfutsal/ThanhfutsalApplication.java
@@ -0,0 +1,13 @@
+package thanhfutsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanhfutsalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThanhfutsalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/thanhfutsal/ThanhfutsalApplicationTests.java
@@ -0,0 +1,13 @@
+package thanhfutsal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThanhfutsalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
